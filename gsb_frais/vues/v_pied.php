bbcdb5147ca63a38be7954bed9a8d790c1c2c00a
Alexandrafricot <37330519+Alexandrafricot@users.noreply.github.com>
2019-05-21 12:57:41 +0200

    Add files via upload

---
+++ gsb_frais/vues/v_pied.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Vue Pied de page
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    José GIL <jgil@ac-nice.fr>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+?>
+        </div>
+        
+        <script type="text/javascript" src="./scripts/jquery/jquery.min.js"></script>        
+        <script type="text/javascript" src="./scripts/bootstrap/bootstrap.min.js"></script> 
+        <script type="text/javascript" src="./scripts/select2/select2.min.js"></script>
+        <script type="text/javascript" src="./scripts/tablesorter/jquery.tablesorter.min.js"></script>
+        <script type="text/javascript" src="./scripts/tablesorter/jquery.tablesorter.pager.js"></script>
+        <script type="text/javascript" src="./scripts/tablesorter/jquery.tablesorter.widgets.js"></script>
+        <script type="text/javascript" src="./scripts/composantsFormulaires.js"></script>
+        <script type="text/javascript" src="./scripts/tableauDynamique.js"></script>
+       
+    </body>
+</html>
+
+
